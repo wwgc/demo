dbe424f8f1d0162cc55ec7b87a46a2cb1985225d
jian <j60017268@gmail.com>
2016-05-15 10:02:02 +0800

    Update maxsum1.js

--- maxsum1.js
+++ maxsum1.js
@@ -5,7 +5,7 @@ module.exports = function(arr){
 	for(let i = 0; i < arr.length; i ++){
 		for(let j = i; j<arr.length; j ++){
 			let sum = 0;
-			for(let k=i;k<j; k++){
+			for(let k=i; k<j; k++){
 				sum += arr[k];
 			}
 			maxsofar = Math.max(maxsofar, sum);
