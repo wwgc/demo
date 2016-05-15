c641007ef00fb374d1334567ad038dbda59ee472
jian <j60017268@gmail.com>
2016-05-15 10:43:20 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,9 +1,30 @@
 'use strict';
 
+const maxsum1 = require('./maxsum1');
+const maxsum21 = require('./maxsum21');
+const maxsum22 = require('./maxsum22');
 const maxsum3 = require('./maxsum3');
 const maxsum4 = require('./maxsum4');
 
 let a=[1,2,-1,5,-4,2,3,-5,6,4,10,-9];
+
+console.time('maxsum1');
+
+console.log(maxsum1(a));
+
+console.timeEnd('maxsum1');
+
+console.time('maxsum21');
+
+console.log(maxsum21(a));
+
+console.timeEnd('maxsum21');
+
+console.time('maxsum22');
+
+console.log(maxsum22(a));
+
+console.timeEnd('maxsum22');
 
 console.time('maxsum3');
 
