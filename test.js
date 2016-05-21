1e3f4bd9f89b0f432a6a61a80667e53aabcb99be
jian <j60017268@gmail.com>
2016-05-21 22:14:29 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,39 +1,10 @@
 'use strict';
 
-const maxsum1 = require('./maxsum1');
-//const maxsum21 = require('./maxsum21');
-//const maxsum22 = require('./maxsum22');
-//const maxsum3 = require('./maxsum3');
-//const maxsum4 = require('./maxsum4');
+const qsort1 = require('./qsort1');
 
-let a=[1,2,-1,5,-4,2,3,-5,6,4,10,-9];
+let arr=[1,2,-1,5,-4,2,3,-5,6,4,10,-9];
 
-console.time('maxsum1');
-
-console.log(maxsum1(a));
-
-console.timeEnd('maxsum1');
-
-//console.time('maxsum21');
-
-//console.log(maxsum21(a));
-
-//console.timeEnd('maxsum21');
-
-//console.time('maxsum22');
-
-//console.log(maxsum22(a));
-
-//console.timeEnd('maxsum22');
-
-//console.time('maxsum3');
-
-//console.log(maxsum3(a, 0, a.length - 1));
-
-//console.timeEnd('maxsum3');
-
-//console.time('maxsum4');
-
-//console.log(maxsum4(a));
-
-//console.timeEnd('maxsum4');
+console.time('qsort1');
+qsort1(arr, 0, arr.length-1);
+console.log(arr);
+console.timeEnd('qsort1');
