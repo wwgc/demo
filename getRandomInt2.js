0753e734ea1fc80970906449038ca4725d592b65
jian <j60017268@gmail.com>
2016-05-24 18:09:57 +0800

    Create getRandomInt2.js

---
+++ getRandomInt2.js
@@ -0,0 +1,12 @@
+'use strict';
+
+//[0,n),p=m/n
+var getRandomInt2 = function(m, n){
+	let set = new Set();
+	while(set.size < m){
+		set.add(Math.floor(Math.random()*n));
+	}
+	return set;
+}
+
+module.exports = getRandomInt2;
